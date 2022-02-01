754d97dac464736e09948ed38e30756c80cd5b85
RobertHaba <sedkipl98@gmail.com>
2022-02-01 15:12:12 +0100

    Get more message on scroll to top

--- src/components/ChatBody.vue
+++ src/components/ChatBody.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="h-[calc(100vh-8rem)] p-2 pb-14 sm:p-2 md:p-6 overflow-y-auto" id="chatBody">
+  <div
+    class="h-[calc(100vh-8rem)] p-2 pb-14 sm:p-2 md:p-6 overflow-y-auto -translate-x-full slide-to-right"
+    id="chatBody"
+  >
     <template v-for="message in messages" :key="message.id">
       <ChatMessage
-        v-if="messages"
+        v-if="!loading"
         :message="message"
         :scrollToBottomFunction="scrollToBottom"
       />
     </template>
+    <div class="w-full h-full flex items-center justify-center" v-if="loading">
+      <span class="block icon icon-loading animate-spin h-10 w-10"></span>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted, watchEffect, watch, computed } from 'vue';
+import { ref, watch, computed, onMounted } from 'vue';
 import { supabase } from '../supabase/init';
 import ChatMessage from './ChatMessage.vue';
 import { useStore } from 'vuex';
@@ -27,8 +33,12 @@ export default {
       () => store.state.messages[props.channel.id].data
     );
     const messages = ref(null);
-    const getMessagesFromDB = async () => {
-      if (messagesFromStore.value.length < 1) {
+    const msgRange = ref({ start: 0, end: 15 });
+    const loading = ref(true);
+    const loadingMore = ref(false);
+    const scrollProperty = ref({now:0,last:0});
+    const getMessagesFromDB = async (getMore) => {
+      if (messagesFromStore.value.length < 1 || getMore) {
         console.log('Pobieram dane  z bazy - brak wiadomości w store');
         try {
           const { data, error } = await supabase
@@ -36,28 +46,65 @@ export default {
             .select('*')
             .eq('channelID', props.channel.id)
             .order('time', { ascending: false })
-            .limit(15);
+            .range(msgRange.value.start, msgRange.value.end);
           if (error) throw error;
           store.commit('addMessagesFromDB', {
             channelID: props.channel.id,
             messages: data,
+            toEnd: getMore,
           });
         } catch (error) {
           console.log(error);
         }
-      } else {
-        console.log('Pobieram dna z Store');
       }
+      handlerLoadMore(getMore);
       setMessages();
     };
     const setMessages = () => {
-      messages.value = messagesFromStore.value
+      loading.value = false;
+      messages.value = messagesFromStore.value;
     };
     const scrollToBottom = () => {
-      const chatBody = document.querySelector('#chatBody');
       chatBody.scrollTop = chatBody.scrollHeight;
     };
-
+    const handlerLoadMore = (loadMore) => {
+      if (!loadMore) {
+        watchForScrollToTop();
+      } else {
+        console.log(scrollProperty.value);
+        loadingMore.value = false;
+        const checkForDOMLoad = setInterval(() => {
+          if (chatBody.children.length === messages.value.length) {
+            chatBody.scrollTop = scrollProperty.value.now - scrollProperty.value.last;
+            scrollProperty.value.last = scrollProperty.value.now
+            clearInterval(checkForDOMLoad);
+          }
+        }, 50);
+        console.log(chatBody.scrollTop);
+        console.log(chatBody.scrollHeight);
+      }
+    };
+    const watchForScrollToTop = () => {
+      console.log(chatBody);
+      chatBody.addEventListener('scroll', (e) => {
+        if (e.target.scrollTop < 200) {
+          if (!loadingMore.value) {
+            console.log(e.target.scrollTop);
+            console.log(e.target.offsetHeight);
+            loadingMore.value = true;
+            scrollProperty.value.now = e.target.scrollHeight  + e.target.scrollTop;
+            loadMoreMessages();
+          }
+        }
+      });
+    };
+    const loadMoreMessages = () => {
+      const allMsgLength = messages.value.length;
+      console.log(allMsgLength);
+      msgRange.value.start = allMsgLength;
+      msgRange.value.end = allMsgLength + 15;
+      getMessagesFromDB(true);
+    };
     getMessagesFromDB();
 
     watch(
@@ -67,7 +114,10 @@ export default {
         getMessagesFromDB();
       }
     );
-    return { messages, scrollToBottom };
+    onMounted(() => {
+      const chatBody = document.querySelector('#chatBody');
+    });
+    return { messages, scrollToBottom, loading };
   },
 };
 </script>
